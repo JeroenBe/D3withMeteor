5bf205bd959bae9b4f537e43ab25f49ac6f26972
Jeroen <jeroen.berrevoets@gmail.com>
2016-07-07 08:20:29 +0200

    Clean d3 code frome console.logs

--- imports/ui/components/graph.js
+++ imports/ui/components/graph.js
@@ -19,17 +19,18 @@ Template.graph.onRendered(function(){
 
     const svg = d3.select("svg")
     const data = StreamOne.find().fetch()
-        console.log(svg.selectAll("path").attr("d"))
-
+        //Add 0-points to begin and end of graph
         data.unshift({value: 250})
         data.push({value: 250})
+
+        //d3 Linegenerator
         const line = d3.line()
             .x((d, i)=>{return 1000/(data.length-1) * i})
             .y((d)=>{return d.value})
             .curve(d3.curveBasis)
 
+        //Update new path coordinates
         svg.selectAll("path").transition().attr("d", line(data))
-        console.log(svg.selectAll("path").attr("d"))
 
     })
 })
